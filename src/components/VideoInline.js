e389c189e91b81b797a2909268c5401b0ef827de
Wander Tillmann <wanderlopes659@hotmail.com>
2021-04-18 18:33:58 -0300

    primeiro commit

---
+++ src/components/VideoInline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function VideoInline(props) {
+  return (
+    <div className="video-inline">
+      {PaymentResponse.children}
+    </div>
+  );
+}
+
+export default VideoInline;
---
+++ src/services/ApiService.js
@@ -0,0 +1,7 @@
+const api = "http://localhost:3002/api/react-videos/";
+
+export const ApiService = {
+  get(endpoint) {
+    return fetch(`${api}${endpoint}`).then(response => response.json());
+  }
+}
